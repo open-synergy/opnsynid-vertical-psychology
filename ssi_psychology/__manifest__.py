05e21bb6b19afa2376d740914f31c6a27bbb9271
Andhitia Rama <andhitia.r@gmail.com>
2022-03-19 20:37:13 +0700

    [ssi_psychology] 14.0.2.0.0
    * Add demo data for: (1) res.partner, and (2) res.users
    * Add demo data for: (1) res.partner, and (2) res.users

--- ssi_psychology/__manifest__.py
+++ ssi_psychology/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Psychology",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -12,9 +12,14 @@
     "application": True,
     "depends": [
         "configuration_helper",
+        "mail",
     ],
     "data": [
         "menu.xml",
         "views/res_config_settings_views.xml",
+    ],
+    "demo": [
+        "demo/res_partner_demo.xml",
+        "demo/res_users_demo.xml",
     ],
 }
