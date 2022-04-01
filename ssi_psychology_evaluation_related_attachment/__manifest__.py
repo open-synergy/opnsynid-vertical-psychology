300bbee17b5ea5e43e2cc731fdc838adf3f29738
Andhitia Rama <andhitia.r@gmail.com>
2022-04-01 23:02:25 +0700

    [ssi_psychology_evaluation_related_attachment] 14.0.1.0.0

---
+++ ssi_psychology_evaluation_related_attachment/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Psychology Evaluation - Related Attachment Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_psychology_evaluation",
+        "ssi_related_attachment_mixin",
+    ],
+    "data": [
+        "views/psychology_evaluation_views.xml",
+        "views/psychology_evaluation_batch_views.xml",
+    ],
+}
