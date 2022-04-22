6ed5ba8216f515b46bcb8585433f2d172563f591
Andhitia Rama <andhitia.r@gmail.com>
2022-04-23 00:16:12 +0700

    [ssi_psychology_evaluation] 14.0.2.5.0
    * Record rules implementation

--- ssi_psychology_evaluation/__manifest__.py
+++ ssi_psychology_evaluation/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Psychology Evaluation",
-    "version": "14.0.2.4.0",
+    "version": "14.0.2.5.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -21,6 +21,7 @@
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
         "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
         "data/ir_sequence_data.xml",
         "data/sequence_template_data.xml",
         "data/approval_template_data.xml",
