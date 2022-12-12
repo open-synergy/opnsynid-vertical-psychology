a348eb6bc8e5a6c82680ab7a0f93841aa72cd1a8
Andhitia Rama <andhitia.r@gmail.com>
2022-12-12 07:04:13 +0700

    [ssi_psychology_case_outsource_work] 14.0.1.0.0

---
+++ ssi_psychology_case_outsource_work/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Psychology Case - Outsource Work Integration",
+    "version": "14.0.1.1.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_psychology_case",
+        "ssi_outsource_work",
+    ],
+    "data": [],
+}
