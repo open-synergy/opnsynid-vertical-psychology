f450aa9d75675d5c5e792ee1f3893fec14beac5b
Andhitia Rama <andhitia.r@gmail.com>
2022-12-22 11:42:10 +0700

    [ssi_psychology_testing_outsource_work] 14.0.1.0.0

---
+++ ssi_psychology_testing_outsource_work/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    psychology_testing,
+    psychology_test,
+    psychology_testing_observer,
+)
