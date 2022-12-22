c2fbbf77be0827fcaebcb871b6b93b9f0e8d5219
Andhitia Rama <andhitia.r@gmail.com>
2022-12-22 11:42:10 +0700

    [ssi_psychology_testing_outsource_work] 14.0.1.0.0

---
+++ ssi_psychology_testing_outsource_work/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Psychology Testing - Outsource Work Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_psychology_testing",
+        "ssi_outsource_work",
+    ],
+    "data": [
+        "views/psychology_test_views.xml",
+        "views/psychology_testing_views.xml",
+    ],
+}
