d185d1b331cbbddb4f4551525ef3d4d88b0611e4
Andhitia Rama <andhitia.r@gmail.com>
2023-01-08 17:11:18 +0700

    [ssi_psychology_testing_outsource_work] 14.0.1.1.0

--- ssi_psychology_testing_outsource_work/models/psychology_testing.py
+++ ssi_psychology_testing_outsource_work/models/psychology_testing.py
@@ -14,6 +14,8 @@ class PsychologyTesting(models.Model):
         "mixin.outsource_work_object",
     ]
     _outsource_work_create_page = True
+    _work_log_page_xpath = "//page[@name='note']"
+    _work_log_template_position = "before"
 
     outsource_work_id = fields.Many2one(
         string="# Outsource Work",
